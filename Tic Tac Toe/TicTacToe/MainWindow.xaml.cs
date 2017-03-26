87deeb15446aa45ba1ec90cb474744cc55441acf
Caravello <567362@bah.com>
2017-03-26 13:25:28 -0500

    Added homework 5

---
+++ Tic Tac Toe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int turn;
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            turn = 1;
+        }
+
+        
+
+        private void win(string btnContent)
+        {
+            
+            
+        }
+        
+
+
+        private void disablebuttons()
+        {
+            foreach (Button btn in uxGrid.Children)
+            {
+                btn.IsEnabled = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (turn == 1)
+            {
+                btn.Content = "O";
+                
+            }
+            else
+            {
+                btn.Content = "X";
+                
+            }
+            btn.IsEnabled = false;
+            win(btn.Content.ToString());
+            turn += 1;
+            if (turn > 2)
+                turn = 1;
+        }
+        private void uxNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button btn in uxGrid.Children)
+            {
+                btn.Content = "";
+                btn.IsEnabled = true;
+            }
+        }
+       
+    }
+}
